6d5d16401fb3c25a41d144b9ecd398e8b2ce14cc
Rodrigo Bossini <professorbossini@gmail.com>
2024-10-28 15:45:48 -0300

    ap09(redux): encerra a apostila

--- cashback_com_redux/index.js
+++ cashback_com_redux/index.js
@@ -1,3 +1,4 @@
+const Redux = require('redux')
 //função criadora de ação: ela cria novos contratos
 const criarContrato = (nome, taxa) => {
   //ela devolve uma ação, ou seja, um objeto JS
@@ -36,3 +37,42 @@ const historicoDePedidosDeCashback = (historicoDePedidosDeCashbackAtual = [], ac
   }
   return historicoDePedidosDeCashbackAtual
 }
+
+const caixa = (dinheiroEmCaixa = 0, acao) => {
+  if(acao.type === "CASHBACK"){
+    dinheiroEmCaixa -= acao.payload.valor
+  }
+  else if (acao.type === "CRIAR_CONTRATO"){
+    dinheiroEmCaixa += acao.payload.taxa
+  }
+  return dinheiroEmCaixa    
+}
+
+const contratos = (listaDeContratosAtual = [], acao) => {
+  if(acao.type === "CRIAR_CONTRATO")
+    return [...listaDeContratosAtual, acao.payload]
+  if(acao.type === "CANCELAR_CONTRATO")
+    return listaDeContratosAtual.filter(c => c.nome !== acao.payload.nome)
+  return listaDeContratosAtual
+}
+
+const { createStore, combineReducers } = Redux
+
+const todosOsReducers = combineReducers({
+  historicoDePedidosDeCashback, caixa, contratos
+})
+
+const store = createStore(todosOsReducers)
+
+const acaoContratoJose = criarContrato('José', 50)
+store.dispatch(acaoContratoJose)
+console.log(store.getState())
+const acaoContratoMaria = criarContrato('Maria', 50)
+store.dispatch(acaoContratoMaria)
+console.log(store.getState())
+const acaoCashbackMaria = solicitarCashback('Maria', 10)
+store.dispatch(acaoCashbackMaria)
+console.log(store.getState())
+const acaoCancelaContratoMaria = cancelarContrato('Maria')
+store.dispatch(acaoCancelaContratoMaria)
+console.log(store.getState())
